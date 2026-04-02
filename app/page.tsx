1d9f9909b5e3ed4a71fb5c7ef9f892dc07d838e8
Sunny <sunnypatel93@hotmail.co.uk>
2026-04-02 17:47:39 -0400

    Change Homepage and added Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 export default function Page() {
   return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Homepage</h1>
-      <p>This is Homepage</p>
+    <main className="
+      min-h-screen 
+      flex flex-col 
+      items-center 
+      justify-start 
+      pt-2 
+      text-center 
+      font-sans
+    ">
+      
+      <img
+        src="/logo2.png"
+        alt="Sunny Patel Photography"
+        className="w-full max-w-[700px]"
+      />
+
+      <h1 className="text-xl md:text-3xl font-semibold mt-4">
+        Homepage
+      </h1>
+
     </main>
   );
 }
